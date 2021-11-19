8c17bf2dc9e3446542573f83748df1c617da1ed6
p81238109 <39752409+p81238109@users.noreply.github.com>
2021-11-19 09:14:50 +0800

    Update UrlConstant.java
    新增一些钉盘接口路径

--- backend/src/main/java/com/dingtalk/constant/UrlConstant.java
+++ backend/src/main/java/com/dingtalk/constant/UrlConstant.java
@@ -6,7 +6,7 @@ package com.dingtalk.constant;
 public class UrlConstant {
 
     /**
-     * 获取access_token url
+     * 获取企业内部应用access_token url
      */
     public static final String GET_ACCESS_TOKEN_URL = "https://oapi.dingtalk.com/gettoken";
 
@@ -14,8 +14,25 @@ public class UrlConstant {
      * 通过免登授权码获取用户信息 url
      */
     public static final String GET_USER_INFO_URL = "https://oapi.dingtalk.com/topapi/v2/user/getuserinfo";
+    
     /**
      * 根据用户id获取用户详情 url
      */
     public static final String USER_GET_URL = "https://oapi.dingtalk.com/topapi/v2/user/get";
+    
+    /**
+     * 获取第三方企业应用get_corp_token url
+     */
+    public static final String GET_CORP_TOKEN_URL = "https://oapi.dingtalk.com/service/get_corp_token";
+    
+    /**
+     * 获取企业下的自定义空间get_custom_space url
+     */
+    public static final String GET_CUSTOM_SPACE_URL = "https://oapi.dingtalk.com/cspace/get_custom_space";
+    
+    /**
+     * 授权用户访问企业的自定义空间grant_custom_space url
+     */
+    public static final String GRANT_CUSTOM_SPACE_URL = "https://oapi.dingtalk.com/cspace/grant_custom_space";
+    
 }
